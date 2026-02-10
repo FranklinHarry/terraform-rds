7d87cf3b29906ed7dcc4d6155f8fec97b39bc7c1
Simon Rondelez <simonrondelez@me.com>
2026-02-10 09:15:30 +0100

    chore: expose ARN of cluster

--- aurora/outputs.tf
+++ aurora/outputs.tf
@@ -1,23 +1,27 @@
 output "aurora_cluster_id" {
-  value = "${aws_rds_cluster.aurora.id}"
+  value = aws_rds_cluster.aurora.id
+}
+
+output "aurora_cluster_arn" {
+  value = aws_rds_cluster.aurora.arn
 }
 
 output "aurora_cluster_instances_id" {
-  value = "${aws_rds_cluster_instance.cluster_instances.*.id}"
+  value = aws_rds_cluster_instance.cluster_instances.*.id
 }
 
 output "aurora_port" {
-  value = "${local.port}"
+  value = local.port
 }
 
 output "aurora_sg_id" {
-  value = "${aws_security_group.sg_aurora.id}"
+  value = aws_security_group.sg_aurora.id
 }
 
 output "endpoint" {
-  value = "${aws_rds_cluster.aurora.endpoint}"
+  value = aws_rds_cluster.aurora.endpoint
 }
 
 output "reader_endpoint" {
-  value = "${aws_rds_cluster.aurora.reader_endpoint}"
+  value = aws_rds_cluster.aurora.reader_endpoint
 }
